20a1a27ad5f11df33785b7dce42945deb104c875
samueljimenezdev <sajimenezr1@gmail.com>
2025-05-19 19:59:48 -0500

    Add docker compose with mongo, create tasks resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('tasks')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
