86fbb386331c4763fe0c075a93938db02e73567c
samueljimenezdev <sajimenezr1@gmail.com>
2025-05-22 19:45:49 -0500

    Add mongo to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { TasksModule } from './tasks/tasks.module';
 import { ExpenseTrackerModule } from './expense-tracker/expense-tracker.module';
 import { BudgetModule } from './budget/budget.module';
 import { ListsModule } from './lists/lists.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [TasksModule, ExpenseTrackerModule, BudgetModule, ListsModule],
+  imports: [
+    TasksModule, 
+    ExpenseTrackerModule, 
+    BudgetModule, 
+    ListsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/plan-life'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
