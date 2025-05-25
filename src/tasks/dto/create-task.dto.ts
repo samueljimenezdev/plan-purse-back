1632e88e0a87078897ee282d98879919895a9310
samueljimenezdev <sajimenezr1@gmail.com>
2025-05-25 11:28:50 -0500

    Create DTO for tasks@

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,39 @@
-export class CreateTaskDto {}
+import { IsDateString, IsIn, IsInt, IsNotEmpty, IsString, MaxLength } from "class-validator";
+import { StatusEnum } from "../entities-mongo/task.entity";
+
+export class CreateTaskDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(500)
+    description: string;
+
+    @IsDateString()
+    dueDate: Date;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsIn(Object.values(StatusEnum))
+    status: string;
+
+    @IsNotEmpty()
+    @IsInt()
+    @IsIn([1, 2, 3, 4, 5])
+    priority: number;
+
+    @IsDateString()
+    @IsNotEmpty()
+    createdAt: Date;
+
+    @IsString({ each: true })
+    tags: string[];
+
+    @IsInt()
+    @IsNotEmpty()
+    version: number;
+}
